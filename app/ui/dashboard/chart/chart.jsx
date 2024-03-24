19bc9e7178f074e67c18d67a9cf7be00b86c6217
GidyMane <muragegideon2000@gmail.com>
2024-03-24 12:12:31 +0300

    Update

--- app/ui/dashboard/chart/chart.jsx
+++ app/ui/dashboard/chart/chart.jsx
@@ -1,72 +1,46 @@
 "use client"
 
-import styles from './chart.module.css'
-import { LineChart, Line, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import styles from './chart.module.css';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const data = [
-  {
-    name: "Sun",
-    visit: 4000,
-    click: 2400,
-  },
-  {
-    name: "Mon",
-    visit: 3000,
-    click: 1398,
-  },
-  {
-    name: "Tue",
-    visit: 2000,
-    click: 3800,
-  },
-  {
-    name: "Wed",
-    visit: 2780,
-    click: 3908,
-  },
-  {
-    name: "Thu",
-    visit: 1890,
-    click: 4800,
-  },
-  {
-    name: "Fri",
-    visit: 2390,
-    click: 3800,
-  },
-  {
-    name: "Sat",
-    visit: 3490,
-    click: 4300,
-  },
+  { month: "JAN", activities: 100 },
+  { month: "FEB", activities: 200 },
+  { month: "MAR", activities: 150 },
+  { month: "APR", activities: 300 },
+  { month: "MAY", activities: 250 },
+  { month: "JUN", activities: 400 },
+  { month: "JUL", activities: 350 },
+  { month: "AUG", activities: 200 },
+  { month: "SEP", activities: 300 },
+  { month: "OCT", activities: 250 },
+  { month: "NOV", activities: 180 },
+  { month: "DEC", activities: 220 }
 ];
 
 const Chart = () => {
   return (
     <div className={styles.container}>
-      <h2 className={styles.title}>Weekly Recap</h2>
+      <h2 className={styles.title}>Monthly Activities</h2>
       <ResponsiveContainer width="100%" height="90%">
-        <LineChart
-          width={500}
-          height={300}
+        <BarChart
           data={data}
           margin={{
-            top: 5,
+            top: 20,
             right: 30,
             left: 20,
-            bottom: 5,
+            bottom: 20,
           }}
         >
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip contentStyle={{background:"#151c2c", border:"none"}}/>
+          <XAxis dataKey="month" />
+          <YAxis domain={[0, 400]} />
+          <Tooltip contentStyle={{ background: "#151c2c", border: "none" }} />
           <Legend />
-          <Line type="monotone" dataKey="visit" stroke="#8884d8" strokeDasharray="5 5" />
-          <Line type="monotone" dataKey="click" stroke="#82ca9d" strokeDasharray="3 4 5 2" />
-        </LineChart>
+          <Bar dataKey="activities" fill="#8884d8" />
+        </BarChart>
       </ResponsiveContainer>
     </div>
-  )
-}
+  );
+};
 
-export default Chart
+export default Chart;
